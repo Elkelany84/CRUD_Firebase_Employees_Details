c746a342ecb74e3f6c11a1c99e92cf6696c629fb
Elkelany84 <drnet80@gmail.com>
2023-12-30 10:30:00 +0200

    first commit

---
+++ lib/model/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const TextStyle dTextStyle =
+    TextStyle(color: Colors.blue, fontSize: 24, fontWeight: FontWeight.bold);
---
+++ lib/pages/home.dart
@@ -0,0 +1,41 @@
+import 'package:crud_firebase_employees_details/model/constants.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Flutter",
+              style: dTextStyle,
+            ),
+            Text(
+              "Firebase",
+              style: dTextStyle.copyWith(color: Colors.orange),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
