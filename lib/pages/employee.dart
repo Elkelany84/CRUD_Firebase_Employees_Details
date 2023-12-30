df0758a5c71c2f04f16ddaf51d870a900efb8859
Elkelany84 <drnet80@gmail.com>
2023-12-30 11:05:33 +0200

    20Minute

--- lib/pages/employee.dart
+++ lib/pages/employee.dart
@@ -1,5 +1,6 @@
 import 'package:crud_firebase_employees_details/model/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Employee extends StatefulWidget {
   const Employee({super.key});
@@ -40,12 +41,62 @@ class _EmployeeState extends State<Employee> {
               height: 10,
             ),
             Container(
+              padding: EdgeInsets.only(left: 10),
               decoration: BoxDecoration(
                   border: Border.all(),
                   borderRadius: BorderRadius.circular(10)),
               child: TextField(
                 decoration: InputDecoration(border: InputBorder.none),
               ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Age",
+              style: dTextStyle.copyWith(color: Colors.black),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10)),
+              child: TextField(
+                decoration: InputDecoration(border: InputBorder.none),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Location",
+              style: dTextStyle.copyWith(color: Colors.black),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10)),
+              child: TextField(
+                decoration: InputDecoration(border: InputBorder.none),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Add",
+                    style: dTextStyle,
+                  )),
             )
           ],
         ),
